f5e98177d888dfa4b5569b942747f08bae5a4bd8
priyanka1683 <priyanka100927@gmail.com>
2026-01-12 16:23:49 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  instance_type = "t3.micro"
+  ami = "ami-0360c520857e3138f" # change this
+  subnet_id = "subnet-0b82ed5723da90554" # change this
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "first-s3-demo-xyz" # change this
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
